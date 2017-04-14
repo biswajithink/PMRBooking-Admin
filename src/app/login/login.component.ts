55df6379f705e53e7561a19c3daa3a5321a1af16
pradip647 <pradipmondal7777@gmail.com>
2017-04-14 15:32:58 +0530

    login and component authentication done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import {Router} from '@angular/router';
+import * as firebase from 'firebase';
 
 /**
 *	This class represents the lazy loaded LoginComponent.
@@ -13,13 +14,32 @@ import {Router} from '@angular/router';
 })
 
 export class LoginComponent { 
+	public email:string;
+	public password:string;
 	
 constructor(private router: Router) { }
 
-goHome(){
+doLogin(){
+	console.log(this.email , this.password);
+
+ if(this.email==undefined || this.password == undefined || this.email == "" || this.password == ""){
+        alert("email or password invalid");
+    }else{
+     firebase.auth().signInWithEmailAndPassword(this.email, this.password).then((authenticatedUser) => {
+         console.log("success");
+		 alert("successfully login. Please wait .....")
+     })
+     .catch((error)=>{
+         console.log(error);
+     });
+
+    }
+
+
+
     /*document.getElementById("bodyId").className =
     document.getElementById("bodyId").className.replace('bodyBack','homeBody');*/
-    this.router.navigate(['./booking-home']);
+    //this.router.navigate(['./booking-home']);
 }
 
 }
